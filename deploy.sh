a90837e1c41d63d4dbc88c75d63f41a91a64e403
OneQ1st <129358695+OneQ1st@users.noreply.github.com>
2026-06-27 03:23:52 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -364,10 +364,6 @@ echo -e "${BLUE}─────────────────────�
 GLOBAL_BLOCK="email $MY_EMAIL
     http_port $HTTP_PORT
     https_port $HTTPS_PORT"
-    default_bind [::]
-    servers {
-        trusted_proxies static 0.0.0.0/0 ::/0
-        }
 
 if [ "$USE_EXISTING_CERT" = true ]; then
     cat <<CADDY_EOF > /opt/emby-proxy/Caddyfile
@@ -381,7 +377,7 @@ $DOMAIN:$DOMAIN_PORT {
     handle_path /emos* {
         rewrite * /https/video.emos.best/443{path}
         
-        reverse_proxy localhost:8080 {
+        reverse_proxy 127.0.0.1:8080 {
             header_up EMOS-PROXY-ID "eD3VXZD9Ys"
             header_up EMOS-PROXY-NAME "@OneQ1st"
             header_up X-Forwarded-For {remote_host}
@@ -392,7 +388,7 @@ $DOMAIN:$DOMAIN_PORT {
     }
     # 2. 通用路径：保持简单，不带上述多余限制
     handle {
-        reverse_proxy localhost:8080 {
+        reverse_proxy 127.0.0.1:8080 {
             header_up Host {host}
             header_up X-Real-IP {remote_host}
             flush_interval -1
@@ -414,7 +410,7 @@ $DOMAIN:$DOMAIN_PORT {
     handle_path /emos* {
         rewrite * /https/video.emos.best/443{path}
         
-        reverse_proxy localhost:8080 {
+        reverse_proxy 127.0.0.1:8080 {
             header_up EMOS-PROXY-ID "eD3VXZD9Ys"
             header_up EMOS-PROXY-NAME "@OneQ1st"
             header_up X-Forwarded-For {remote_host}
@@ -425,7 +421,7 @@ $DOMAIN:$DOMAIN_PORT {
     }
     # 2. 通用路径：保持简单，不带上述多余限制
     handle {
-        reverse_proxy localhost:8080 {
+        reverse_proxy 127.0.0.1:8080 {
             header_up Host {host}
             header_up X-Real-IP {remote_host}
             flush_interval -1
@@ -447,7 +443,7 @@ $DOMAIN:$DOMAIN_PORT {
     handle_path /emos* {
         rewrite * /https/video.emos.best/443{path}
         
-        reverse_proxy localhost:8080 {
+        reverse_proxy 127.0.0.1:8080 {
             header_up EMOS-PROXY-ID "eD3VXZD9Ys"
             header_up EMOS-PROXY-NAME "@OneQ1st"
             header_up X-Forwarded-For {remote_host}
@@ -458,7 +454,7 @@ $DOMAIN:$DOMAIN_PORT {
     }
     # 2. 通用路径：保持简单，不带上述多余限制
     handle {
-        reverse_proxy localhost:8080 {
+        reverse_proxy 127.0.0.1:8080 {
             header_up Host {host}
             header_up X-Real-IP {remote_host}
             flush_interval -1
